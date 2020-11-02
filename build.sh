8aeca992b6316480d0017abbdd361a1cc68c5aee
kookehs <4277184+kookehs@users.noreply.github.com>
2020-11-02 12:32:58 -0800

    bug: remove building for darwin on arm64

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # GOOS=darwin GOARCH=386 go build -o bdo-marketplace-darwin32 &&
-GOOS=darwin GOARCH=arm64 go build -o bdo-marketplace-darwin-arm64 &&
+# GOOS=darwin GOARCH=arm64 go build -o bdo-marketplace-darwin-arm64 &&
 GOOS=darwin GOARCH=amd64 go build -o bdo-marketplace-darwin-amd64 &&
 GOOS=linux GOARCH=386 go build -o bdo-marketplace-linux-x8632 &&
 GOOS=linux GOARCH=amd64 go build -o bdo-marketplace-linux-amd64 &&
