4faba581ae5ffe5a366f97b1b04e322dd55712c0
kookehs <4277184+kookehs@users.noreply.github.com>
2020-12-20 18:06:50 -0800

    bug: fix mistach in naming between build and README

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 GOOS=darwin GOARCH=arm64 go build -o bdo-marketplace-darwin-arm64 &&
 GOOS=darwin GOARCH=amd64 go build -o bdo-marketplace-darwin-amd64 &&
-GOOS=linux GOARCH=386 go build -o bdo-marketplace-linux-x8632 &&
+GOOS=linux GOARCH=386 go build -o bdo-marketplace-linux-386 &&
 GOOS=linux GOARCH=amd64 go build -o bdo-marketplace-linux-amd64 &&
-GOOS=windows GOARCH=386 go build -o bdo-marketplace-windows-x8632.exe &&
+GOOS=windows GOARCH=386 go build -o bdo-marketplace-windows-386.exe &&
 GOOS=windows GOARCH=amd64 go build -o bdo-marketplace-windows-amd64.exe
